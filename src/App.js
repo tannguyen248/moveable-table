83dec7e76c82aef21f49406e2de89f03d767ab70
Tan Nguyen <tannguyen@blueberrylabs.com>
2021-09-17 10:13:45 +0700

    implement innerbound

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
+import init from "./MoveableController";
 
 function App() {
+  useEffect(() => {
+    const moveable = init();
+    const inner = document.querySelector(".inner");
+    const bound = inner.getBoundingClientRect();
+    moveable.innerBounds = {
+      left: bound.left,
+      top: bound.top,
+      width: bound.width,
+      height: bound.height,
+    };
+    // const target = document.querySelector(".target");
+    // const bound = target.getBoundingClientRect();
+    // moveable.target = document.querySelector(".target");
+    // moveable.innerBounds = {
+    //   left: bound.x,
+    //   top: bound.y,
+    //   width: 10,
+    //   height: 10,
+    // };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="target"></div>
+      <div
+        className="inner"
+        style={{
+          width: 50,
+          height: 50,
+          border: "1px solid",
+          top: 150,
+          left: 150,
+          position: "absolute",
+        }}
+      >
+        bound
+      </div>
     </div>
   );
 }
---
+++ src/MoveableController.js
@@ -0,0 +1,74 @@
+import Moveable from "moveable";
+
+const init = () => {
+  const moveable = new Moveable(document.body, {
+    // If you want to use a group, set multiple targets(type: Array<HTMLElement | SVGElement>).
+    target: document.querySelector(".target"),
+    snappable: true,
+    draggable: true,
+    throttleDrag: 0,
+    startDragRotate: 0,
+    throttleDragRotate: 0,
+    zoom: 1,
+    origin: true,
+    padding: { left: 0, top: 0, right: 0, bottom: 0 },
+
+    resizable: true,
+    keepRatio: true,
+    throttleResize: 0,
+
+    rotatable: true,
+    throttleRotate: 0,
+    rotationPosition: "top",
+  });
+
+  let frame = {
+    translate: [0, 0],
+    rotate: 0,
+    clipStyle: "inset",
+  };
+
+  moveable
+    .on("dragStart", (e) => {
+      e.set(frame.translate);
+    })
+    .on("drag", (e) => {
+      console.log("top: ", e.top, "left: ", e.left);
+      console.log(e.clientX, e.clientY);
+
+      frame.translate = e.beforeTranslate;
+      e.target.style.transform = `translate(${e.beforeTranslate[0]}px, ${e.beforeTranslate[1]}px)`;
+    })
+    .on("clip", (e) => {
+      if (e.clipType === "rect") {
+        e.target.style.clip = e.clipStyle;
+      } else {
+        e.target.style.clipPath = e.clipStyle;
+      }
+      frame.clipStyle = e.clipStyle;
+    })
+    .on("resizeStart", (e) => {
+      e.setOrigin(["%", "%"]);
+      e.dragStart && e.dragStart.set(frame.translate);
+    })
+    .on("resize", (e) => {
+      const beforeTranslate = e.drag.beforeTranslate;
+
+      frame.translate = beforeTranslate;
+      e.target.style.width = `${e.width}px`;
+      e.target.style.height = `${e.height}px`;
+      e.target.style.transform = `translate(${beforeTranslate[0]}px, ${beforeTranslate[1]}px)`;
+    })
+    .on("rotateStart", (e) => {
+      e.set(frame.rotate);
+      e.dragStart && e.dragStart.set(frame.translate);
+    })
+    .on("rotate", (e) => {
+      frame.rotate = e.beforeRotate;
+      e.target.style.transform = `rotate(${e.beforeRotate}deg)`;
+    });
+
+  return moveable;
+};
+
+export default init;
